7a5a53421cb3c6f44ce96bfb79cf791df14fc6e7
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-03-28 16:25:46 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let flag = args[1].clone();
+    let threads = args[2].clone();
+    ler ipaddr = args[3].clone();
+
+}
